f4c585603c3c0b645e591b8c030ce74cabc02169
Purvansh Gourh <purvansh.g@pharmeasy.in>
2020-06-01 15:35:32 +0530

    Updates

---
+++ precache-manifest.2352f5fbb143a84142f73ea70af42d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc8fff74dcc5a284ea3ed77fc8ec68d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ce65ef682adc18425520",
+    "url": "/my-portfolio/static/css/2.bb083c27.chunk.css"
+  },
+  {
+    "revision": "1aae03d463b4f0c1847a",
+    "url": "/my-portfolio/static/css/main.d132d55b.chunk.css"
+  },
+  {
+    "revision": "ce65ef682adc18425520",
+    "url": "/my-portfolio/static/js/2.1fd164c0.chunk.js"
+  },
+  {
+    "revision": "ac4a1bef3ce26e0dcc346b3cc4225ab8",
+    "url": "/my-portfolio/static/js/2.1fd164c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aae03d463b4f0c1847a",
+    "url": "/my-portfolio/static/js/main.4aebc13d.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  }
+]);
